71e083a918b2ebdf7f49bb5035de792611da7c1b
Jon Ballands <jballands@gmail.com>
2017-09-29 14:55:27 -0500

    Fixed the loading animation

--- src/js/components/LoadingAnimation.jsx
+++ src/js/components/LoadingAnimation.jsx
@@ -6,41 +6,132 @@
 //
 
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
+
+const LoadingContainer = styled.div`
+	display: inline-flex;
+	flex-flow: column nowrap;
+	align-items: center;
+	width: 250px;
+`;
+
+const Text = styled.div`
+	color: ${props => props.color};
+	font-size: 13px;
+	margin-top: 5px;
+`;
 
 const LoadingAnimationContainer = styled.div`
 	width: 100%;
 	max-width: 300px;
 	height: 3px;
+	position: relative;
 `;
 
 const Animation = styled.div`
-	@keyframes growerLeft {
+	position: absolute;
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	background: ${props => props.color};
+	height: 100%;
+	width: 100%;
+	animation-iteration-count: infinite;
+	animation-duration: ${props => props.duration}s;
+	animation-timing-function: ease;
+`;
+
+const AnimationLeft = Animation.extend`
+	@keyframes bounceLeft {
 		0% {
 			transform: scaleX(0);
-			transform-origin: left;
+			opacity: 1;
 		}
 		25% {
 			transform: scaleX(1);
-			transform-origin: left;
+			opacity: 1;
+		}
+		26% {
+			opacity: 0;
+		}
+		75% {
+			opacity: 0;
+		}
+		76% {
+			opacity: 1;
+			transform: scaleX(1);
+		}
+		100% {
+			transform: scaleX(0);
+			opacity: 1;
 		}
 	}
 
-	background: black;
-	height: 100%;
-	width: 100%;
+	animation-name: bounceLeft;
+	transform-origin: 0% 50%;
+`;
+
+const AnimationRight = Animation.extend`
+	@keyframes bounceRight {
+		0% {
+			transform: scaleX(1);
+			opacity: 0;
+		}
+		25% {
+			opacity: 0;
+		}
+		26% {
+			transform: scaleX(1);
+			opacity: 1;
+		}
+		50% {
+			transform: scaleX(0);
+			opacity: 1;
+		}
+		75% {
+			transform: scaleX(1);
+			opacity: 1;
+		}
+		76% {
+			opacity: 0;
+		}
+		100% {
+			opacity: 0;
+		}
+	}
+
+	transform-origin: 100% 50%;
+	animation-name: bounceRight;
 `;
 
 export default class LoadingAnimation extends React.Component {
 	static displayName = 'LoadingAnimation';
 
-	static propTypes = {};
+	static propTypes = {
+		color: PropTypes.string,
+		duration: PropTypes.number,
+		text: PropTypes.string,
+	};
+
+	static defaultProps = {
+		color: 'black',
+		duration: 1.7,
+		text: 'Wait a sec',
+	};
 
 	render() {
+		const { color, duration, text } = this.props;
+
 		return (
-			<LoadingAnimationContainer>
-				<Animation />
-			</LoadingAnimationContainer>
+			<LoadingContainer>
+				<LoadingAnimationContainer>
+					<AnimationLeft color={color} duration={duration} />
+					<AnimationRight color={color} duration={duration} />
+				</LoadingAnimationContainer>
+				{text && <Text color={color}>{text}</Text>}
+			</LoadingContainer>
 		);
 	}
 }
