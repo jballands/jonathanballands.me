1f4a8d27e01a74a80dd7225f32052c109983bc3e
Jonathan Ballands <jballands@gmail.com>
2018-07-06 00:34:31 -0500

    Updating to latest

--- experiments/AppleTVParalax/index.jsx
+++ experiments/AppleTVParalax/index.jsx
@@ -8,7 +8,7 @@ const Grid = styled.div`
 	display: flex;
 	flex-flow: row wrap;
 	align-items: flex-start;
-	justify-content: space-around;
+	justify-content: center;
 	width: 100%;
 `;
 
@@ -28,6 +28,12 @@ const LayerAbove = styled.img`
 	height: 105%;
 `;
 
+const StyledAppleTVIcon = styled(AppleTVIcon)`
+	width: 250px;
+	height: 250px;
+	margin: 15px;
+`;
+
 export default class AppleTVParalax extends Component {
 	static displayName = 'AppleTVParalax';
 
@@ -38,8 +44,7 @@ export default class AppleTVParalax extends Component {
 	render() {
 		return (
 			<Grid>
-				<AppleTVIcon
-					style={{ height: 250, width: 250 }}
+				<StyledAppleTVIcon
 					parallaxMultiplier={0.06}
 					layers={[
 						<Layer src="/assets/singing-saw-3.png" />,
@@ -47,20 +52,40 @@ export default class AppleTVParalax extends Component {
 						<LayerAbove src="/assets/singing-saw-1.png" />,
 					]}
 				/>
-				<AppleTVIcon
-					style={{ height: 250, width: 250 }}
+				<StyledAppleTVIcon
 					parallaxMultiplier={0.065}
 					layers={[
 						<Layer src="/assets/1975-2.png" />,
 						<Layer src="/assets/1975-1.png" />,
 					]}
 				/>
-				<AppleTVIcon
-					style={{ height: 250, width: 250 }}
+				<StyledAppleTVIcon
 					layers={[
 						<Layer src="/assets/tranquility-base-hotel-and-casino-3.png" />,
 						<Layer src="/assets/tranquility-base-hotel-and-casino-2.png" />,
 						<Layer src="/assets/tranquility-base-hotel-and-casino-1.png" />,
+					]}
+				/>
+				<StyledAppleTVIcon
+					parallaxMultiplier={0.06}
+					layers={[
+						<Layer src="/assets/99-cents-3.png" />,
+						<Layer src="/assets/99-cents-2.png" />,
+						<Layer src="/assets/99-cents-1.png" />,
+					]}
+				/>
+				<StyledAppleTVIcon
+					layers={[
+						<Layer src="/assets/this-old-dog-3.png" />,
+						<Layer src="/assets/this-old-dog-2.png" />,
+						<Layer src="/assets/this-old-dog-1.png" />,
+					]}
+				/>
+				<StyledAppleTVIcon
+					layers={[
+						<Layer src="/assets/chronovision-3.png" />,
+						<Layer src="/assets/chronovision-2.png" />,
+						<Layer src="/assets/chronovision-1.png" />,
 					]}
 				/>
 			</Grid>
